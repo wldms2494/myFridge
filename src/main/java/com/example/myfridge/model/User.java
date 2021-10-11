0cd9b96cf5e73515587030dee55fd4b7e29fe565
Jieun Song <80088918+wldms2494@users.noreply.github.com>
2021-10-11 21:26:51 +0900

    로그인 회원가입 기능 구현 완료, 유효성 검사 전

--- src/main/java/com/example/myfridge/model/User.java
+++ src/main/java/com/example/myfridge/model/User.java
@@ -1,8 +1,41 @@
 package com.example.myfridge.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
 public class User {
 
-
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
     private Long id;
 
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    //unique 는 중복 허용 여부,비밀번호는 중복 허용
+    @Column(nullable = false, unique = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+//로그인 폼을 위한 생성자
+    public User(String username, String password, String email, UserRoleEnum role){
+
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
 }
---
+++ src/main/java/com/example/myfridge/service/UserService.java
@@ -0,0 +1,56 @@
+package com.example.myfridge.service;
+
+import com.example.myfridge.dto.SignupRequestDto;
+import com.example.myfridge.model.User;
+import com.example.myfridge.model.UserRoleEnum;
+import com.example.myfridge.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private static final String ADMIN_TOKEN = "AAABnv/xRVklrnYxKZ0aHgTBcXukeZygoC";
+
+
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+    //회원등록
+    public void registerUser(SignupRequestDto requestDto) {
+        String username = requestDto.getUsername();
+    //ID중복 처리 부분
+    //optional 사용, 조건문 없이도 null값 의 예외처리 가능
+        Optional<User> found = userRepository.findByUsername(username);
+        if(found.isPresent()){
+           throw new IllegalArgumentException("중복된 사용자 ID가 존재합니다.");
+        }
+
+    //패스워드 암호화
+        String password = passwordEncoder.encode(requestDto.getPassword());
+
+    // 이메일
+        String email = requestDto.getEmail();
+
+    //사용자 ROLE확인
+        UserRoleEnum role = UserRoleEnum.USER;
+        if(requestDto.isAdmin()){
+            if (!requestDto.getAdminToken().equals(ADMIN_TOKEN)) {
+                throw new IllegalArgumentException(("관리자 암호가 틀려 등록이 불가합니다.")) ;
+            }
+            role = UserRoleEnum.ADMIN;
+        }
+
+        User user = new User(username, password, email, role);
+        userRepository.save(user);
+
+    }
+}
