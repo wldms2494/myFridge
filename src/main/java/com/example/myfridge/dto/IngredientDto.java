a3a01218d4e71d169e3fb45ce2e8464f251ee735
KimSeonkyum <rlatjsrua02@naver.com>
2021-10-16 11:04:02 +0900

    test code 진행중

--- src/main/java/com/example/myfridge/dto/IngredientDto.java
+++ src/main/java/com/example/myfridge/dto/IngredientDto.java
@@ -5,4 +5,8 @@ import lombok.Getter;
 @Getter
 public class IngredientDto {
     private String ingredient;
+
+    public IngredientDto(String ingredient) {
+        this.ingredient = ingredient;
+    }
 }
